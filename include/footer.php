2e0895f13225edc45567dc42196920c346873e98
Alexgamerlvb1234 <86124474+Alexgamerlvb1234@users.noreply.github.com>
2021-09-23 17:29:58 +0100

    test

--- include/footer.php
+++
@@ -1,5 +0,0 @@
-<div class="col-md-12"><center>	<br /><hr />	<footer>
-			
-			&copy; <script type="text/javascript"> document.write(new Date().getFullYear()) </script> <strong><?php echo $website ?></strong> Copyright | All rights reserved.
-		
-		</footer></center></div>
